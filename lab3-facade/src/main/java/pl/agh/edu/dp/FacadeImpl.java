5f6e281d3925087699ca2959cab19f56c2254065
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2022-11-17 10:56:16 +0100

    after lab

---
+++ lab3-facade/src/main/java/pl/agh/edu/dp/FacadeImpl.java
@@ -0,0 +1,9 @@
+package pl.agh.edu.dp;
+
+public class FacadeImpl implements Facade{
+    NetworkManager networkManager = new NetworkManagerImpl();
+
+    void sendMessage(){
+        Message msg = new ...
+    }
+}
